1d2fea15c1fde1170091af566e967238d5725fd9
Abdullah297846 <shazainali45@gmail.com>
2024-09-14 21:03:28 +0500

    final_update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Abdullah nAdeem',
+    author_email= 'abdullahnadeem1@outlook.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
